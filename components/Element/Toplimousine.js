48ee85400eec7e4dfc0a657c2b349252f10b2105
oscarking1 <o.king@invictusstudio.com>
2021-06-05 02:09:45 +0500

    top links

--- components/Element/Toplimousine.js
+++ components/Element/Toplimousine.js
@@ -8,22 +8,22 @@ class Toplimousine extends Component{
                     <h4>Our Top Limousine Service Locations</h4>
                     <p>Grand Limousine & Chauffeur Service Top Locations</p>
                     <ul>
-                        <li><a href="/locations/augusta/">Airport Transportation in Augusta, Georgia</a></li>
-                        <li><a href="/locations/houston/">Airport Transportation in Houston, Texas</a></li>
-                        <li><a href="/locations/destin/">Airport Transportation in Destin, Florida</a></li>
-                        <li><a href="/locations/los-angeles/">Airport Transportation in Los Angeles, California</a></li>
-                        <li><a href="/locations/jacksonville/">Airport Transportation in Jacksonville, Florida</a></li>
-                        <li><a href="/locations/macon/">Airport Transportation in Macon, Georgia</a></li>
-                        <li><a href="/locations/orlando/">Airport Transportation in Orlando, Florida</a></li>
-                        <li><a href="/locations/san-francisco/">Airport Transportation in San Francisco, California</a></li>
-                        <li><a href="/locations/new-york/">Airport Transportation in NewYork</a></li>
-                        <li><a href="/locations/gray/">Airport Transportation in Gray, Georgia</a></li>
-                        <li><a href="/locations/chicago/">Airport Transportation in Chicago, Illinois</a></li>
-                        <li><a href="/locations/columbus/">Airport Transportation in Columbus, Georgia</a></li>
-                        <li><a href="/locations/miami/">Airport Transportation in Miami, Florida</a></li>
-                        <li><a href="/locations/atlanta/">Airport Transportation in Atlanta, Georgia</a></li>
-                        <li><a href="/locations/savannah/">Airport Transportation in Savannah, Georgia</a></li>
-                        <li><a href="/locations/palm-beach/">Airport Transportation in Palm Beach, Florida</a></li>
+                        <li><a href="/locations/augusta-car-service/">Airport Transportation in Augusta, Georgia</a></li>
+                        <li><a href="/locations/houston-car-service/">Airport Transportation in Houston, Texas</a></li>
+                        <li><a href="/locations/destin-car-service/">Airport Transportation in Destin, Florida</a></li>
+                        <li><a href="/locations/los-angeles-car-service/">Airport Transportation in Los Angeles, California</a></li>
+                        <li><a href="/locations/jacksonville-car-service/">Airport Transportation in Jacksonville, Florida</a></li>
+                        <li><a href="/locations/macon-car-service/">Airport Transportation in Macon, Georgia</a></li>
+                        <li><a href="/locations/orlando-car-service/">Airport Transportation in Orlando, Florida</a></li>
+                        <li><a href="/locations/san-francisco-car-service/">Airport Transportation in San Francisco, California</a></li>
+                        <li><a href="/locations/new-york-car-service/">Airport Transportation in NewYork</a></li>
+                        <li><a href="/locations/gray-car-service/">Airport Transportation in Gray, Georgia</a></li>
+                        <li><a href="/locations/chicago-car-service/">Airport Transportation in Chicago, Illinois</a></li>
+                        <li><a href="/locations/columbus-car-service/">Airport Transportation in Columbus, Georgia</a></li>
+                        <li><a href="/locations/miami-car-service/">Airport Transportation in Miami, Florida</a></li>
+                        <li><a href="/locations/atlanta-car-service/">Airport Transportation in Atlanta, Georgia</a></li>
+                        <li><a href="/locations/savannah-car-service/">Airport Transportation in Savannah, Georgia</a></li>
+                        <li><a href="/locations/palm-beach-car-service/">Airport Transportation in Palm Beach, Florida</a></li>
                         <li><a href="/locations/kentucky/">Airport Transportation in Kentucky</a></li>
                     </ul>
                 </div>
