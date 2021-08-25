e3468c4bc46fbc7d6b0e0329949f6fb82815fd06
JacksonPaulHeck <jacksonheck1@gmail.com>
2021-08-25 16:54:25 -0500

    Update google tag manager

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ export default class MyDocument extends Document {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5P942W4');`,
+            })(window,document,'script','dataLayer','GTM-58T7FQN');`,
           }}>
           </script>
           <script type='application/ld+json' dangerouslySetInnerHTML={{
@@ -76,8 +76,8 @@ export default class MyDocument extends Document {
         </Head>
         <body>
           <noscript dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5P942W4"
-height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-58T7FQN"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
           <Main />
           <NextScript />
         </body>
